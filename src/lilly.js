2c118fdd6c6bb0d4cd81ddb7991d0b197be0fa75
Facegrinder <szauer.erik@gmail.com>
2019-10-05 14:16:02 +0200

    Changed docs, Added Lilly.js, Added init function

---
+++ src/lilly.js
@@ -0,0 +1,15 @@
+/**
+ * The main Lilly class
+ */
+class Lilly {
+    constructor() {
+
+    }
+
+    /**
+     * Init the Lilly class
+     */
+    init() {
+        console.log('Initializing LillyJS');
+    }
+}
